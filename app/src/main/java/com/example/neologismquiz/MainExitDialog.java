6d00d163796640559666aa99ff95d99cd5a44a73
hsyoodev <hsyoodev@gmail.com>
2024-01-08 14:30:54 +0900

    게임시작 뷰 게임종료 대화상자

--- app/src/main/java/com/example/neologismquiz/MainExitDialog.java
+++ app/src/main/java/com/example/neologismquiz/MainExitDialog.java
@@ -6,17 +6,19 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
-public class MainDialog extends Dialog {
+public class MainExitDialog extends Dialog {
     private final Activity activity;
+    private Window window;
     private Button positiveButton, cancelButton;
-    private TextView message;
+    private TextView messageText;
 
-    public MainDialog(@NonNull Activity activity) {
+    public MainExitDialog(@NonNull Activity activity) {
         super(activity);
         this.activity = activity;
     }
@@ -25,15 +27,18 @@ public class MainDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog);
-        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        // 커스텀 대화상자 윈도우 배경색 지정
+        window = getWindow();
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         positiveButton = findViewById(R.id.positiveButton);
         cancelButton = findViewById(R.id.cancelButton);
-        message = findViewById(R.id.message);
-
+        messageText = findViewById(R.id.messageText);
+        
         positiveButton.setText("취소다룡");
         cancelButton.setText("종료다룡");
-        message.setText("앱을 종료할거다룡?");
+        messageText.setText("앱을 종료할거다룡?");
 
         positiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +49,7 @@ public class MainDialog extends Dialog {
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dismiss();
                 activity.finish();
             }
         });
