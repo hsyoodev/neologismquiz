38d4e18a076d5c96d84967e4344ef4b1cc5908a2
hsyoodev <hsyoodev@gmail.com>
2024-01-09 09:52:10 +0900

    스플레시 뷰 구현하기

---
+++ app/src/main/java/com/example/neologismquiz/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.example.neologismquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TextView firstWordText = findViewById(R.id.firstWordText);
+        TextView secondWordText = findViewById(R.id.secondWordText);
+
+        String[] appNames = getResources().getString(R.string.app_name).split(" ");
+        
+        firstWordText.setText(appNames[0]);
+        secondWordText.setText(appNames[1]);
+
+        // 애니메이션
+        Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        };
+        Animation tingtingAnimation = AnimationUtils.loadAnimation(this, R.anim.tingting);
+        tingtingAnimation.setAnimationListener(animationListener);
+        firstWordText.startAnimation(tingtingAnimation);
+        secondWordText.startAnimation(tingtingAnimation);
+    }
+}
